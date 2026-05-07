4c38995f8afd2c33bbd33ee38661decfa890182c
MarsGotBars <marcinm@live.nl>
2026-05-07 16:35:08 +0200

    migrations

--- app/cms/src/migrations/index.ts
+++ app/cms/src/migrations/index.ts
@@ -1,5 +1,7 @@
 import * as migration_20260507_085451 from './20260507_085451';
 import * as migration_20260507_122001 from './20260507_122001';
+import * as migration_20260507_143204 from './20260507_143204';
+import * as migration_20260507_143346 from './20260507_143346';
 
 export const migrations = [
   {
@@ -10,6 +12,16 @@ export const migrations = [
   {
     up: migration_20260507_122001.up,
     down: migration_20260507_122001.down,
-    name: '20260507_122001'
+    name: '20260507_122001',
+  },
+  {
+    up: migration_20260507_143204.up,
+    down: migration_20260507_143204.down,
+    name: '20260507_143204',
+  },
+  {
+    up: migration_20260507_143346.up,
+    down: migration_20260507_143346.down,
+    name: '20260507_143346'
   },
 ];
