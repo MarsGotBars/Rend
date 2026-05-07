4159e4e6a6e28fb5baf5a73fa44ec9ea64e5bd52
MarsGotBars <marcinm@live.nl>
2026-05-07 14:35:41 +0200

    chore: Add database migration for pages schema updates
    Automatically generated migration that adds:
    - description field to pages
    - thumbImage relationship to images
    - images relationship array to pages
    - Image size variants for multiple formats (webp, avif)

--- app/cms/src/migrations/index.ts
+++ app/cms/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260507_085451 from './20260507_085451';
+import * as migration_20260507_122001 from './20260507_122001';
 
 export const migrations = [
   {
     up: migration_20260507_085451.up,
     down: migration_20260507_085451.down,
-    name: '20260507_085451'
+    name: '20260507_085451',
+  },
+  {
+    up: migration_20260507_122001.up,
+    down: migration_20260507_122001.down,
+    name: '20260507_122001'
   },
 ];
